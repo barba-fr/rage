653a3dc11ee026140a942d024864e0f234ed72ac
sylvain-galoustoff <sylvain.galoustoff@gmail.com>
2022-05-27 00:27:25 +0200

    Page studio commencée

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from './home/Home'
+import Studio from './studio/Studio'
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/studio" element={<Studio />} />
       </Routes>
     </div>
   );
